851bb03ad9c3dfe8c1148f7745ff8292ebc65e45
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-31 15:07:00 +0200

    fixed route

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/projects', [ProjectController::class, 'index']);
 
-Route::get('/project/{project}', [ProjectController::class, 'show']);
+Route::get('/project/{slug}', [ProjectController::class, 'show']);
 
