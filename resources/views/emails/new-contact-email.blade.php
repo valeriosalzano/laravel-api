784126c368996945e190bd63fb305319adcb139c
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-05 19:45:36 +0200

    implemented lead with migration, controller, view, route

---
+++ resources/views/emails/new-contact-email.blade.php
@@ -0,0 +1,9 @@
+Hi <strong>Admin</strong>,
+<br/>
+you have a new contact. Here the details:
+<br/><br/>
+Name: <em>{{$lead->name}}</em>
+<br/>
+Email: <em>{{$lead->email}}</em>
+<br/>
+Message: <em>{{$lead->message}}</em>
